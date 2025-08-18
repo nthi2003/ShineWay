c135ded965fe2dccb3dc2dbb91dd8be1e5e8f64d
nthi2003 <quocthi.dev@gmail.com>
2025-08-18 16:06:07 +0700

    ✨Permission 1.1

--- src/main/java/com/mycompany/myapp/web/rest/AuthorityPermissionResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/AuthorityPermissionResource.java
@@ -12,6 +12,8 @@ import com.mycompany.myapp.service.dto.CreateAuthorityPmDTO;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,23 +29,28 @@ public class AuthorityPermissionResource {
         this.authorityPermissionService = authorityPermissionService;
     }
     @PostMapping("/authorityPermission")
+    @PreAuthorize("hasRole('ADMIN')")
     public AuthorityPermissionDTO createAuthorityPermission(@RequestBody CreateAuthorityPmDTO createAuthorityPmDTO) {
         return authorityPermissionService.createAuthorityPermission(createAuthorityPmDTO);
     }
     @GetMapping("/authorityPermission")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<AuthorityPermissionDTO> getAllAuthorityPermissions() {
         return authorityPermissionService.getAll();
     }
     @PutMapping("/authorityPermission/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public AuthorityPermissionDTO updateAuthorityPermission(@PathVariable String id, @RequestBody AuthorityPermissionDTO authorityPermissionDTO) {
 
         return authorityPermissionService.updateAuthorityPermission(id, authorityPermissionDTO);
     }
     @DeleteMapping("/authorityPermission/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteAuthorityPermission(@PathVariable String id) {
         authorityPermissionService.deleteAuthorityPermission(id);
     }
     @GetMapping("/authorityPermission/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public AuthorityPermissionDTO getAuthorityPermission(@PathVariable String id) {
         return authorityPermissionService.getAuthorityPermission(id);
     }
